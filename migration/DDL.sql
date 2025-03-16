3629dc53de2d778cfb266419ca420f7a32cb2b1d
phenomejo <taksinjo@hotmail.com>
2025-03-16 15:52:39 +0700

    update ddl script

--- migration/DDL.sql
+++ migration/DDL.sql
@@ -3,6 +3,7 @@ CREATE TABLE customers (
     first_name VARCHAR(36) NOT NULL,
     last_name VARCHAR(36),
     email VARCHAR(100) NOT NULL,
+    document_id VARCHAR(36) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
